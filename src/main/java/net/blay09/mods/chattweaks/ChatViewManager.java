d1c146ca3abcad8f26702eebb527f47c14c91dca
blay09 <blay09@hotmail.de>
2018-03-23 12:20:00 +0100

    Fix crash on duplicate view name - instead, the names gets (1) etc. appended now
    #43

--- src/main/java/net/blay09/mods/chattweaks/ChatViewManager.java
+++ src/main/java/net/blay09/mods/chattweaks/ChatViewManager.java
@@ -27,213 +27,217 @@ import java.util.Map;
 
 public class ChatViewManager {
 
-	private static final Map<String, ChatView> views = Maps.newHashMap();
-	private static final List<ChatView> sortedViews = Lists.newArrayList();
-	private static String[] viewNames;
-	private static String[] tabViewNames;
-	private static ChatView activeView;
+    private static final Map<String, ChatView> views = Maps.newHashMap();
+    private static final List<ChatView> sortedViews = Lists.newArrayList();
+    private static String[] viewNames;
+    private static String[] tabViewNames;
+    private static ChatView activeView;
 
-	public static ChatView createDefaultView() {
-		ChatView defaultView = new ChatView("*");
-		defaultView.addChannel(ChatManager.mainChannel);
-		defaultView.addChannel(ChatManager.interactionChannel);
-		defaultView.addChannel(ChatManager.systemChannel);
-		defaultView.addChannel(ChatManager.deathChannel);
-		return defaultView;
-	}
+    public static ChatView createDefaultView() {
+        ChatView defaultView = new ChatView("*");
+        defaultView.addChannel(ChatManager.mainChannel);
+        defaultView.addChannel(ChatManager.interactionChannel);
+        defaultView.addChannel(ChatManager.systemChannel);
+        defaultView.addChannel(ChatManager.deathChannel);
+        return defaultView;
+    }
 
-	public static ChatView createSystemView() {
-		ChatView systemView = new ChatView("system");
-		systemView.addChannel(ChatManager.systemChannel);
-		systemView.setMessageStyle(MessageStyle.Side);
-		systemView.setExclusive(true);
-		return systemView;
-	}
+    public static ChatView createSystemView() {
+        ChatView systemView = new ChatView("system");
+        systemView.addChannel(ChatManager.systemChannel);
+        systemView.setMessageStyle(MessageStyle.Side);
+        systemView.setExclusive(true);
+        return systemView;
+    }
 
-	public static ChatView createInteractionView() {
-		ChatView interactionView = new ChatView("interaction");
-		interactionView.addChannel(ChatManager.interactionChannel);
-		interactionView.setMessageStyle(MessageStyle.Bottom);
-		interactionView.setExclusive(true);
-		return interactionView;
-	}
+    public static ChatView createInteractionView() {
+        ChatView interactionView = new ChatView("interaction");
+        interactionView.addChannel(ChatManager.interactionChannel);
+        interactionView.setMessageStyle(MessageStyle.Bottom);
+        interactionView.setExclusive(true);
+        return interactionView;
+    }
 
-	public static ChatView[] createDefaults() {
-		return new ChatView[] {
-				createDefaultView(),
-				createSystemView(),
-				createInteractionView()
-		};
-	}
+    public static ChatView[] createDefaults() {
+        return new ChatView[]{
+                createDefaultView(),
+                createSystemView(),
+                createInteractionView()
+        };
+    }
 
-	public static void load() {
-		removeAllChatViews();
-		Gson gson = new Gson();
-		try(FileReader reader = new FileReader(new File(Minecraft.getMinecraft().mcDataDir, "config/ChatTweaks/views.json"))) {
-			JsonObject root = gson.fromJson(reader, JsonObject.class);
-			JsonArray jsonViews = root.getAsJsonArray("views");
-			for (int i = 0; i < jsonViews.size(); i++) {
-				addChatView(ChatView.fromJson(jsonViews.get(i).getAsJsonObject()));
-			}
-		} catch (FileNotFoundException ignored) {
-		} catch (Exception e) {
-			ChatTweaks.logger.error("An error occurred trying to load the chat views: ", e);
-		}
-		if(views.isEmpty()) {
-			addChatView(createDefaultView());
-			addChatView(createSystemView());
-			addChatView(createInteractionView());
-		}
-		setActiveView(getNextChatView(null, false));
-	}
+    public static void load() {
+        removeAllChatViews();
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader(new File(Minecraft.getMinecraft().mcDataDir, "config/ChatTweaks/views.json"))) {
+            JsonObject root = gson.fromJson(reader, JsonObject.class);
+            JsonArray jsonViews = root.getAsJsonArray("views");
+            for (int i = 0; i < jsonViews.size(); i++) {
+                addChatView(ChatView.fromJson(jsonViews.get(i).getAsJsonObject()));
+            }
+        } catch (FileNotFoundException ignored) {
+        } catch (Exception e) {
+            ChatTweaks.logger.error("An error occurred trying to load the chat views: ", e);
+        }
+        if (views.isEmpty()) {
+            addChatView(createDefaultView());
+            addChatView(createSystemView());
+            addChatView(createInteractionView());
+        }
+        setActiveView(getNextChatView(null, false));
+    }
 
-	public static void save() {
-		Gson gson = new Gson();
-		try(FileWriter writer = new FileWriter(new File(Minecraft.getMinecraft().mcDataDir, "config/ChatTweaks/views.json"))) {
-			JsonWriter jsonWriter = new JsonWriter(writer);
-			jsonWriter.setIndent("  ");
-			JsonObject root = new JsonObject();
-			JsonArray jsonViews = new JsonArray();
-			for(ChatView view : sortedViews) {
-				if(!view.isTemporary()) {
-					jsonViews.add(view.toJson());
-				}
-			}
-			root.add("views", jsonViews);
-			gson.toJson(root, jsonWriter);
-		} catch (IOException e) {
-			ChatTweaks.logger.error("An error occurred trying to save the chat views: ", e);
-		}
-	}
+    public static void save() {
+        Gson gson = new Gson();
+        try (FileWriter writer = new FileWriter(new File(Minecraft.getMinecraft().mcDataDir, "config/ChatTweaks/views.json"))) {
+            JsonWriter jsonWriter = new JsonWriter(writer);
+            jsonWriter.setIndent("  ");
+            JsonObject root = new JsonObject();
+            JsonArray jsonViews = new JsonArray();
+            for (ChatView view : sortedViews) {
+                if (!view.isTemporary()) {
+                    jsonViews.add(view.toJson());
+                }
+            }
+            root.add("views", jsonViews);
+            gson.toJson(root, jsonWriter);
+        } catch (IOException e) {
+            ChatTweaks.logger.error("An error occurred trying to save the chat views: ", e);
+        }
+    }
 
-	private static void updateNameCache() {
-		viewNames = sortedViews.stream().map(ChatView::getName).toArray(String[]::new);
-		tabViewNames = sortedViews.stream().filter(p -> p.getMessageStyle() == MessageStyle.Chat).map(ChatView::getName).toArray(String[]::new);
-	}
+    private static void updateNameCache() {
+        viewNames = sortedViews.stream().map(ChatView::getName).toArray(String[]::new);
+        tabViewNames = sortedViews.stream().filter(p -> p.getMessageStyle() == MessageStyle.Chat).map(ChatView::getName).toArray(String[]::new);
+    }
 
-	public static void addChatView(ChatView view) {
-		if(views.containsKey(view.getName())) {
-			throw new IllegalArgumentException("duplicate view " + view.getName());
-		}
-		views.put(view.getName(), view);
-		sortedViews.add(view);
-		updateNameCache();
+    public static void addChatView(ChatView view) {
+        String origName = view.getName();
+        int c = 1;
+        while (views.containsKey(view.getName())) {
+            view.setName(origName + " (" + c + ")");
+            c++;
+        }
+        views.put(view.getName(), view);
+        sortedViews.add(view);
+        updateNameCache();
 
-		GuiScreen gui = Minecraft.getMinecraft().currentScreen;
-		if(gui instanceof GuiChatExt) {
-			((GuiChatExt) gui).updateChannelButtons();
-		}
-	}
+        GuiScreen gui = Minecraft.getMinecraft().currentScreen;
+        if (gui instanceof GuiChatExt) {
+            ((GuiChatExt) gui).updateChannelButtons();
+        }
+    }
 
-	public static void removeChatView(ChatView view) {
-		views.remove(view.getName());
-		sortedViews.remove(view);
-		if(views.isEmpty()) {
-			ChatView defaultView = createDefaultView();
-			views.put("*", defaultView);
-			sortedViews.add(defaultView);
-		}
-		updateNameCache();
-		if(view == activeView) {
-			setActiveView(getNextChatView(view, false));
-		}
-	}
+    public static void removeChatView(ChatView view) {
+        views.remove(view.getName());
+        sortedViews.remove(view);
+        if (views.isEmpty()) {
+            ChatView defaultView = createDefaultView();
+            views.put("*", defaultView);
+            sortedViews.add(defaultView);
+        }
+        updateNameCache();
+        if (view == activeView) {
+            setActiveView(getNextChatView(view, false));
+        }
+    }
 
-	public static ChatView getNextChatView(@Nullable ChatView view, boolean preferNewMessages) {
-		if(preferNewMessages) {
-			for(ChatView chatView : sortedViews) {
-				if(chatView != view && chatView.hasUnreadMessages()) {
-					return chatView;
-				}
-			}
-		}
-		String[] arr = tabViewNames;
-		if(arr.length == 0) {
-			arr = viewNames;
-		}
-		int index = -1;
-		if(view != null) {
-			index = ArrayUtils.indexOf(arr, view.getName());
-		}
-		index++;
-		if(index >= arr.length) {
-			index = 0;
-		}
-		return views.get(arr[index]);
-	}
+    public static ChatView getNextChatView(@Nullable ChatView view, boolean preferNewMessages) {
+        if (preferNewMessages) {
+            for (ChatView chatView : sortedViews) {
+                if (chatView != view && chatView.hasUnreadMessages()) {
+                    return chatView;
+                }
+            }
+        }
+        String[] arr = tabViewNames;
+        if (arr.length == 0) {
+            arr = viewNames;
+        }
+        int index = -1;
+        if (view != null) {
+            index = ArrayUtils.indexOf(arr, view.getName());
+        }
+        index++;
+        if (index >= arr.length) {
+            index = 0;
+        }
+        return views.get(arr[index]);
+    }
 
-	public static List<ChatView> findChatViews(ChatMessage message, ChatChannel channel) {
-		String unformattedText = message.getTextComponent().getUnformattedText();
-		List<ChatView> result = Lists.newArrayList();
-		for (ChatView view : sortedViews) {
-			if (view.getChannels().contains(channel) && view.messageMatches(unformattedText)) {
-				if (view.isExclusive()) {
-					result.clear();
-					result.add(view);
-					message.setExclusiveView(view);
-					break;
-				}
-				result.add(view);
-			}
-		}
-		return result;
-	}
+    public static List<ChatView> findChatViews(ChatMessage message, ChatChannel channel) {
+        String unformattedText = message.getTextComponent().getUnformattedText();
+        List<ChatView> result = Lists.newArrayList();
+        for (ChatView view : sortedViews) {
+            if (view.getChannels().contains(channel) && view.messageMatches(unformattedText)) {
+                if (view.isExclusive()) {
+                    result.clear();
+                    result.add(view);
+                    message.setExclusiveView(view);
+                    break;
+                }
+                result.add(view);
+            }
+        }
+        return result;
+    }
 
-	public static void setActiveView(ChatView view) {
-		activeView = view;
-		view.markAsUnread(false);
-		ChatTweaks.getChatDisplay().refreshChat();
-	}
+    public static void setActiveView(ChatView view) {
+        activeView = view;
+        view.markAsUnread(false);
+        ChatTweaks.getChatDisplay().refreshChat();
+    }
 
-	public static ChatView getActiveView() {
-		return activeView;
-	}
+    public static ChatView getActiveView() {
+        return activeView;
+    }
 
-	public static Collection<ChatView> getViews() {
-		return sortedViews;
-	}
+    public static Collection<ChatView> getViews() {
+        return sortedViews;
+    }
 
-	@Nullable
-	public static ChatView getChatView(String name) {
-		return views.get(name);
-	}
+    @Nullable
+    public static ChatView getChatView(String name) {
+        return views.get(name);
+    }
 
-	@Deprecated
-	public static ChatView getOrCreateChatView(String name) {
-		ChatView chatView = getChatView(name);
-		if(chatView == null) {
-			chatView = new ChatView(name);
-			addChatView(chatView);
-		}
-		return chatView;
-	}
+    @Deprecated
+    public static ChatView getOrCreateChatView(String name) {
+        ChatView chatView = getChatView(name);
+        if (chatView == null) {
+            chatView = new ChatView(name);
+            addChatView(chatView);
+        }
+        return chatView;
+    }
 
-	private static List<String> reservedNames = Lists.newArrayList();
-	public static String getFreeChatViewName() {
-		String baseName = "New View";
-		String name = baseName;
-		int i = 0;
-		while(views.containsKey(name) || reservedNames.contains(name)) {
-			i++;
-			name = baseName + " (" + i + ")";
-		}
-		reservedNames.add(name);
-		return name;
-	}
+    private static List<String> reservedNames = Lists.newArrayList();
 
-	public static void removeAllChatViews() {
-		views.clear();
-		sortedViews.clear();
-		viewNames = new String[0];
-		tabViewNames = new String[0];
-		reservedNames.clear();
-	}
+    public static String getFreeChatViewName() {
+        String baseName = "New View";
+        String name = baseName;
+        int i = 0;
+        while (views.containsKey(name) || reservedNames.contains(name)) {
+            i++;
+            name = baseName + " (" + i + ")";
+        }
+        reservedNames.add(name);
+        return name;
+    }
 
-	public static void renameChatView(ChatView chatView, String name) {
-		views.remove(chatView.getName());
-		sortedViews.remove(chatView);
-		chatView.setName(name);
-		addChatView(chatView);
-	}
+    public static void removeAllChatViews() {
+        views.clear();
+        sortedViews.clear();
+        viewNames = new String[0];
+        tabViewNames = new String[0];
+        reservedNames.clear();
+    }
+
+    public static void renameChatView(ChatView chatView, String name) {
+        views.remove(chatView.getName());
+        sortedViews.remove(chatView);
+        chatView.setName(name);
+        addChatView(chatView);
+    }
 
 }
